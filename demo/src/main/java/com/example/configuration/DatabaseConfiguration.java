b91be9fbcf364ad6f3e1a2eb3fb9fc2f80d9ad0c
user1 <user1@DESKTOP-QD9ND2F>
2020-06-15 20:52:21 +0900

    123

---
+++ demo/src/main/java/com/example/configuration/DatabaseConfiguration.java
@@ -0,0 +1,28 @@
+package com.example.configuration;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+public class DatabaseConfiguration {
+
+//	@Bean
+//	public DataSource dataSource() {
+//		BasicDataSource source = new BasicDataSource();
+//		
+////		source.setDriverClassName("oracle.jdbc.OracleDriver");
+////		source.setUrl("jdbc:oracle:thin:@localhost:1521:xe");
+////		source.setUsername(userName);
+////		source.setPassword(password);
+////		
+////		source.setMaxTotal(maxTotal);
+////		source.setMaxIdle(maxIdle);
+////		source.setMaxWaitMillis(maxWaitMillis);
+////		
+//		
+//	}
+	
+}
---
+++ demo/src/main/java/com/example/demo/entity/Book.java
@@ -0,0 +1,42 @@
+package com.example.demo.entity;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity @SequenceGenerator(name = "book_seq", sequenceName = "book_seq", initialValue = 1, allocationSize = 1)
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+	
+	@Id // 기본키 설정
+	@GeneratedValue(generator = "book_seq")  //시퀀스 적용
+	private int no;
+	@Column(nullable = false, length =60)
+	private String name;
+	@Column(nullable = false, length =60)
+	private String publisher;
+	
+	@Column
+	private int price;
+	
+	//생성 시각
+	private Timestamp ctime;
+	
+	//수정 시각
+	private Timestamp utime;
+	
+	
+}
